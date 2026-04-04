692d0a28c51da520f87dba35294c5d6eb287dbe4
Str-F <175103213+Str-F@users.noreply.github.com>
2026-04-04 19:23:48 +0200

    fixed some last bugs

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,17 +1,8 @@
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Analytics } from '@vercel/analytics/react';
-import { Outlet, useLocation } from 'react-router-dom';
-import { Button } from '@/components/ui/button';
-import {
-  SunMediumIcon as Sun,
-  MoonIcon as Moon
-} from "@/components/icons";
+import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
-  const location = useLocation();
-  const isLanding = location.pathname === "/" || location.pathname === "/auth";
-  const isEditor = location.pathname === "/editor";
-
   return (
     <section>
         <Outlet />
